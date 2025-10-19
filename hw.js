01eab149087fc1ed4b251e2e9bbb4667b380c686
NIKITAPRO965 <nikita.pro.965@gmail.com>
2025-10-19 13:34:23 +0300

    .

--- hw.js
+++ hw.js
@@ -90,8 +90,8 @@ const users = [
 // //масив.reduce((acc,item,idx,arr)=>{},0)
 
 //Отримати загальну суму балансу (поле balance) всіх користувачів.
-// const calculateTotalBalance = users => users.reduce((acc, total) =>  acc + total.balance, 0)
-// console.log(calculateTotalBalance(users)); // 20916
+const calculateTotalBalance = users => users.reduce((acc, total) =>  acc + total.balance, 0)
+console.log(calculateTotalBalance(users)); // 20916
 
 
 
@@ -102,12 +102,10 @@ const users = [
 
 //Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
 
-// const getUsersWithFriend = (users, friendName) => {
-//   return users.filter(user => user.friends.includes(friendName)).map(user => user.name);
-// };
+const getUsersWithFriend = (users, friendName) => users.filter(({friends}) => friends.includes(friendName)).map(({name}) => name);
 
-// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 
 
@@ -121,8 +119,8 @@ const users = [
 
 // Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
 
-// const getNamesSortedByFriendsCount = users => [...users].sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
-// console.log(getNamesSortedByFriendsCount(users));
+const getNamesSortedByFriendsCount = users => [...users].sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
+console.log(getNamesSortedByFriendsCount(users));
 // // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 
@@ -140,9 +138,14 @@ const users = [
 
 // Отримати масив всіх умінь всіх користувачів (поле skills), при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
 
-const getSortedUniqueSkills = users => {
-
-};
+const getSortedUniqueSkills = users => users.flatMap(({skills}) => skills).filter((item, idx, arr) => arr.indexOf(item) === idx).sort((a, b) => a.localeCompare(b))
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+
+
+
+
+
+
 
